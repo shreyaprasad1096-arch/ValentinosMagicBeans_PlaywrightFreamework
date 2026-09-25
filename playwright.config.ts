b28449bc8a7abc0e681d89c8b6dcb040ad2494b6
Shreya Prasad <shreyaprasad@Shreyas-MacBook-Air.local>
2026-09-26 01:46:02 +0530

    added screenshot trace and video to defineconfig

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,9 +28,10 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
      baseURL: 'https://valentinos-magic-beans.click/',
-
+     screenshot: 'only-on-failure',
+     video: 'retain-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
     headless: process.env.headless === "true"
   },
 
